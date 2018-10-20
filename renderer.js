193b6b7bbf0483c60a0b6c414a4c8df7399f69e9
svmi3195 <target1983@gmail.com>
2018-10-20 19:26:49 +0300

    add event listeners

---
+++ renderer.js
@@ -0,0 +1,12 @@
+
+window.document.getElementById('countries').addEventListener('click', function(){
+  window.document.getElementById('test').innerText = 'countries';
+});
+
+window.document.getElementById('bestiary').addEventListener('click', function(){
+  window.document.getElementById('test').innerText = 'bestiary';
+});
+
+window.document.getElementById('characters').addEventListener('click', function(){
+  window.document.getElementById('test').innerText = 'characters';
+});
