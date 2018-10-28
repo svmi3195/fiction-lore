b7acd200829f1cad948ded01c70dc17fd12924e4
svmi3195 <target1983@gmail.com>
2018-10-28 17:16:50 +0200

    test using module

---
+++ js/test.js
@@ -0,0 +1,1 @@
+exports.testing = function(){console.log('testing')}
--- renderer.js
+++ renderer.js
@@ -1,3 +1,4 @@
+const testing = require('./js/test.js')
 
 window.document.getElementById('countries').addEventListener('click', function(){
   window.document.getElementById('test').innerText = 'countries';
@@ -10,3 +11,7 @@ window.document.getElementById('bestiary').addEventListener('click', function(){
 window.document.getElementById('characters').addEventListener('click', function(){
   window.document.getElementById('test').innerText = 'characters';
 });
+
+window.document.getElementById('test').append(document.createElement('div'))
+
+testing.testing();
