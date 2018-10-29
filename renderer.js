7a48066d25ae1179fc3dff10bff4de60c82ea0b5
svmi3195 <target1983@gmail.com>
2018-10-29 20:25:03 +0200

    cleanup

--- renderer.js
+++ renderer.js
@@ -2,7 +2,7 @@ const data = require('./data1/index.json')
 
 
 window.document.getElementById('countries').addEventListener('click', function () {
-  const container = window.document.getElementById('test');
+  const container = window.document.getElementById('main');
   const list = window.document.createElement('ul');
 
   data.countries.forEach(element => {
@@ -13,7 +13,7 @@ window.document.getElementById('countries').addEventListener('click', function (
 });
 
 window.document.getElementById('bestiary').addEventListener('click', function () {
-  const container = window.document.getElementById('test');
+  const container = window.document.getElementById('main');
   const list = window.document.createElement('ul');
 
   data.bestiary.forEach(element => {
@@ -24,7 +24,7 @@ window.document.getElementById('bestiary').addEventListener('click', function ()
 });
 
 window.document.getElementById('characters').addEventListener('click', function () {
-  const container = window.document.getElementById('test');
+  const container = window.document.getElementById('main');
   const list = window.document.createElement('ul');
 
   data.characters.forEach(element => {
