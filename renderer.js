4acf79f776aae476730b54f502133fd7282b00b1
svmi3195 <target1983@gmail.com>
2018-10-30 20:14:23 +0200

    some refactoring

--- renderer.js
+++ renderer.js
@@ -1,39 +1,22 @@
 const data = require('./data1/index.json')
 
-
 window.document.getElementById('countries').addEventListener('click', function () {
-  const container = window.document.getElementById('main');
-  const list = window.document.createElement('ul');
-
-  data.countries.forEach(element => {
-    let listItem = window.document.createElement('li');
-    listItem.innerText = element.name;
-    list.append(listItem);
-  });
-
-  container.innerHTML = '';
-  container.append(list);
+  createList(data.countries);
 });
 
 window.document.getElementById('bestiary').addEventListener('click', function () {
-  const container = window.document.getElementById('main');
-  const list = window.document.createElement('ul');
-
-  data.bestiary.forEach(element => {
-    let listItem = window.document.createElement('li');
-    listItem.innerText = element.name;
-    list.append(listItem);
-  });
-
-  container.innerHTML = '';
-  container.append(list);
+  createList(data.bestiary);
 });
 
 window.document.getElementById('characters').addEventListener('click', function () {
+  createList(data.characters);
+});
+
+function createList(dataArray){
   const container = window.document.getElementById('main');
   const list = window.document.createElement('ul');
 
-  data.characters.forEach(element => {
+  dataArray.forEach(element => {
     let listItem = window.document.createElement('li');
     listItem.innerText = element.name;
     list.append(listItem);
@@ -41,6 +24,4 @@ window.document.getElementById('characters').addEventListener('click', function 
 
   container.innerHTML = '';
   container.append(list);
-});
-
-
+}
